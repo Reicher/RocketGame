ab6da262cd9f4ddf3adbeb322249b7618b45980d
JHelar <john.helarsson@gmail.com>
2020-11-23 22:43:11 +0100

    Added hud background and fuel label

--- src/scenes/hud.ts
+++ src/scenes/hud.ts
@@ -5,9 +5,7 @@ export interface HuDCreateOptions {
     player: Ship
 }
 
-const hudOptions = {
-    spacing: 20
-}
+const MIN_HEALTH_THRESHOLD = 0.25
 
 const barOptions = {
     height: 35,
@@ -17,19 +15,20 @@ const barOptions = {
     minFillColor: 0x8C1515
 }
 
+const hudOptions = {
+    spacing: 20,
+    textStyle: { font: `bold ${barOptions.height - 20}px monospace`, fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle" }
+}
+
 export default class HuDScene extends Phaser.Scene {
     player: Ship
     healthBar: Phaser.GameObjects.Graphics
-    healthBarX: number
-    healthBarY: number
+    healthBarX = 0
+    healthBarY = 0
     healthText: Phaser.GameObjects.Text
 
     constructor() {
         super({})
-
-        this.healthBarX = hudOptions.spacing
-        this.healthBarY = config.height - barOptions.height - hudOptions.spacing
-
         this.player = {} as Ship
         this.healthBar = {} as Phaser.GameObjects.Graphics
         this.healthText = {} as Phaser.GameObjects.Text
@@ -38,24 +37,32 @@ export default class HuDScene extends Phaser.Scene {
     preload(){
         console.log('HuD - Preload')
     }
-    create(data: HuDCreateOptions) {
-        console.log('HuD - Create')
+    create({ player }: HuDCreateOptions) {
+        this.player = player
 
-        this.player = data.player
+        const hudBackgroundHeight = barOptions.height + hudOptions.spacing * 2
+        const hudBackground = this.add.graphics()
+        hudBackground.fillStyle(0x333333, 0.87)
+        hudBackground.fillRect(0, config.height - hudBackgroundHeight, config.width, hudBackgroundHeight)
+        
+        const fuelLabel = this.add.text(0, 0, 'Fuel', hudOptions.textStyle)
+        this.healthBarX = hudOptions.spacing * 2 + fuelLabel.displayWidth
+        this.healthBarY = config.height - barOptions.height - hudOptions.spacing
 
         this.healthBar = this.add.graphics()
 
-        this.healthText = this.add.text(0, 0, `${this.player.maxFuel} / ${this.player.maxFuel}`, { font: `bold ${barOptions.height - 20}px monospace`, fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle", maxWidth: 200 })
+        this.healthText = this.add.text(0, 0, `${this.player.maxFuel} / ${this.player.maxFuel}`, hudOptions.textStyle)
         this.healthText.setPosition(this.healthBarX + barOptions.width / 2 - this.healthText.displayWidth / 2, this.healthBarY + barOptions.height / 2 - this.healthText.displayHeight / 2)
+        fuelLabel.setPosition(hudOptions.spacing, this.healthText.y)
+        
 
         this.updateHealth({ current: this.player.maxFuel, max: this.player.maxFuel })
-
         this.player.addListener(EVENTS.UPDATE_HEALTH, this.updateHealth.bind(this))
     }
 
     updateHealth({ current, max }: UpdateHealthPayload) {
         const percentageFuel = current / max
-        const healthColor = percentageFuel < 0.25 ? barOptions.minFillColor : barOptions.maxFillColor
+        const healthColor = percentageFuel < MIN_HEALTH_THRESHOLD ? barOptions.minFillColor : barOptions.maxFillColor
         const barWidth = Math.floor(barOptions.width * percentageFuel)
 
         this.healthText.setText(`${current} / ${max}`)
@@ -63,7 +70,7 @@ export default class HuDScene extends Phaser.Scene {
         this.healthBar.clear()
         
         this.healthBar.lineStyle(2, Phaser.Display.Color.ValueToColor(healthColor).darken(25).color)
-        this.healthBar.strokeRect(this.healthBarX, this.healthBarY, barOptions.width, barOptions.height + 2)
+        this.healthBar.strokeRect(this.healthBarX, this.healthBarY, barOptions.width + 2, barOptions.height + 2)
         
         this.healthBar.fillStyle(healthColor)
         this.healthBar.fillRect(this.healthBarX + 1, this.healthBarY + 1, barWidth, barOptions.height)
